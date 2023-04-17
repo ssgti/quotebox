eb31c950990502bea7f4ec7b2a5b791b7142b272
ssgti <williampaulgeorge@outlook.com>
2023-04-17 16:55:24 +0100

    modimfications

--- test1/dailyQuote.cs
+++ test1/dailyQuote.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 using System.Text.Json;
 using System.Net;
-using System.Threading.Tasks;
 
 namespace test1
 {
--- test1/inputWindow.cs
+++ test1/inputWindow.cs
@@ -1,1 +1,31 @@
-﻿
+﻿namespace test1
+{
+    class inputWindow : Form
+    {
+        string inputText;
+        
+        public inputWindow()
+        {
+            InitializeComponent();
+        }
+
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label searchLabel;
+
+        private void InitializeComponent()
+        {
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Text = "stock quote endpoint";
+
+            this.SuspendLayout();
+
+            inputBox = new TextBox();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.inputBox.Location = new System.Drawing.Point(10, 30);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(60, 20);
+            this.inputBox.Text = this.inputText;
+        }
+    }
+}
