c9c6d652e14b21f6ae20ceef5a03d8c52ab43a4b
ssgti <williampaulgeorge@outlook.com>
2023-04-19 16:35:02 +0100

    add user input window

--- test1/Program.cs
+++ test1/Program.cs
@@ -4,11 +4,8 @@ namespace test1
     {
         static void Main()
         {
-            string symbol = "INVP.L";
-            dailyQuote stock = new dailyQuote(symbol);
-
             ApplicationConfiguration.Initialize();
-            Application.Run(new quoteWindow(stock));
+            Application.Run(new inputWindow());
         }
     }
 
--- test1/inputWindow.cs
+++ test1/inputWindow.cs
@@ -10,8 +10,8 @@
         }
 
         private System.Windows.Forms.TextBox inputBox;
-        private System.Windows.Forms.Button searchButton;
-        private System.Windows.Forms.Label searchLabel;
+        private System.Windows.Forms.Button quoteBtn;
+        private System.Windows.Forms.Label quoteLabel;
 
         private void InitializeComponent()
         {
@@ -24,8 +24,41 @@
             this.inputBox = new System.Windows.Forms.TextBox();
             this.inputBox.Location = new System.Drawing.Point(10, 30);
             this.inputBox.Name = "inputBox";
-            this.inputBox.Size = new System.Drawing.Size(60, 20);
-            this.inputBox.Text = this.inputText;
+            this.inputBox.Size = new System.Drawing.Size(100, 30);
+            Controls.Add(inputBox);
+
+            quoteBtn = new Button();
+            this.quoteBtn = new System.Windows.Forms.Button();
+            this.quoteBtn.Location = new System.Drawing.Point(10, 60);
+            this.quoteBtn.Text = "get quote";
+            this.quoteBtn.Size = new System.Drawing.Size(100, 25);
+            quoteBtn.Click += new EventHandler(quoteBtn_Click);
+            Controls.Add(quoteBtn);
+
+            quoteLabel = new Label();
+            this.quoteLabel = new System.Windows.Forms.Label();
+            this.quoteLabel.Location = new System.Drawing.Point(7, 10);
+            this.quoteLabel.AutoSize = true;
+            this.quoteLabel.Size = new System.Drawing.Size(100, 30);
+            this.quoteLabel.Text = "enter stock symbol";
+            Controls.Add(quoteLabel);
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private void quoteBtn_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                dailyQuote stock = new dailyQuote(inputBox.Text.ToString());
+                quoteWindow quote = new quoteWindow(stock);
+                quote.Show();
+
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "oops!");
+            }
         }
     }
 }
