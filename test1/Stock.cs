2be24ce24bec21acadead043c3dcef76c843dad1
ssgti <williampaulgeorge@outlook.com>
2023-04-25 16:46:34 +0100

    stock can update its own symbol

---
+++ test1/Stock.cs
@@ -0,0 +1,16 @@
+﻿namespace test1
+{
+    class Stock
+    {
+        public string symbol { get; set; }
+        public string name { get; set; }
+
+        public Stock(string symbol)
+        {
+            this.symbol = symbol;
+            nameQuote getData = new nameQuote(symbol);
+            this.symbol = getData.symbol;
+            this.name = getData.name;
+        }
+    }
+}
